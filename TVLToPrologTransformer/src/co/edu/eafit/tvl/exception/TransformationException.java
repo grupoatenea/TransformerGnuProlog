f502b949645d3263961496c08d5613dea3e57b6e
dialtahi <dialtahi@gmail.com>
2013-08-16 00:42:22 -0500

    Capturing TVL parsing exceptions

---
+++ TVLToPrologTransformer/src/co/edu/eafit/tvl/exception/TransformationException.java
@@ -0,0 +1,27 @@
+package co.edu.eafit.tvl.exception;
+
+public class TransformationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TransformationException() {
+		super();
+	}
+
+	public TransformationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public TransformationException(String message) {
+		super(message);
+	}
+
+	public TransformationException(Throwable cause) {
+		super(cause);
+	}
+	
+
+}
